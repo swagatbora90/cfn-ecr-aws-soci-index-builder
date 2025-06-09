be5d1ac0aa312d5f165d0bb8fdd8a81bc7b340eb
Swagat Bora <sbora@amazon.com>
2025-06-09 17:20:25 +0000

    Move all existing Partners Solution documentation to archive/
    Signed-off-by: Swagat Bora <sbora@amazon.com>

--- build_docs.sh
+++ build_docs.sh
@@ -19,20 +19,37 @@ build_guide_with_asciidoc_attributes() {
         ASCIIDOC_ATTRIBUTES="-a production_build"
     fi
     set -x
-    asciidoctor --base-dir docs/ --backend=html5 -o ${OUTPUT_FILE} -w --doctype=book -a toc2 -a current-year=$(date +%Y) ${ASCIIDOC_ATTRIBUTES} ${LAYOUT_FILE}
+    asciidoctor --base-dir ${BASE_DIR} --backend=html5 -o ${OUTPUT_FILE} -w --doctype=book -a toc2 -a current-year=$(date +%Y) ${ASCIIDOC_ATTRIBUTES} ${LAYOUT_FILE}
     set +x
 }
 
+# Generate main documentation
 # path to files showing which type of guide the repo has been configured to display.
 DEPLOYMENTFILE=docs/_deployment_guide.adoc
 
 # Check for deployment guide
 if test -f "$DEPLOYMENTFILE"; then
-    echo "== Generating Deployment Guide =="
+    echo "== Generating Main Deployment Guide =="
 
+    BASE_DIR=docs/
     LAYOUT_FILE=docs/boilerplate/index_deployment_guide.adoc
     OUTPUT_FILE=index.html
 
     build_guide_with_asciidoc_attributes
     mv docs/${OUTPUT_FILE} $(pwd)/
+fi
+
+# Generate archive documentation
+ARCHIVE_DEPLOYMENTFILE=archive/docs/_deployment_guide.adoc
+
+# Check for archive deployment guide
+if test -f "$ARCHIVE_DEPLOYMENTFILE"; then
+    echo "== Generating Archive Deployment Guide =="
+
+    BASE_DIR=archive/docs/
+    LAYOUT_FILE=archive/docs/boilerplate/index_deployment_guide.adoc
+    OUTPUT_FILE=index.html
+
+    build_guide_with_asciidoc_attributes
+    mv archive/docs/${OUTPUT_FILE} ./archive/
 fi
